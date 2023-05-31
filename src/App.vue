fb513194ebfdfd9045ec3eed7c0243e4febb9379
manuelmenama <manuel.mena090589@gmail.com>
2023-05-31 17:39:56 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Principal msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Principal from './components/Principal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Principal
   }
 }
 </script>
---
+++ src/components/Principal.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    <h1>Hi Vue!</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Principal",
+}
+</script>
+<style lang="">
+  
+</style>
