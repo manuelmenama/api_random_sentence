4c3fb42041f73d7fb1081e914e61b747ea5d62d3
manuelmenama <manuel.mena090589@gmail.com>
2023-05-31 22:00:19 +0200

    minor changes

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -4,8 +4,44 @@
   </div>
 </template>
 <script>
+import axios from "axios";
+import {store} from "../data/store";
+
 export default {
   name: "Principal",
+  data() {
+    return {
+      baseUrl: "https://flynn.boolean.careers/exercises/api/random/word",
+      store
+    }
+  },
+  methods: {
+    getOneWord() {
+      axios.get(this.baseUrl)
+        .then(result => {
+          console.log(result.data.response);
+          this.pushInArray(result.data.response);
+        })
+    },
+    pushInArray(word) {
+      store.arrayOfWord.push(word)
+    },
+    arrayDimension(arraySize) {
+      for (let index = 0; index < arraySize; index++) {
+        
+        this.getOneWord();
+        
+      }
+    },
+    logOfArray() {
+      console.log(store.arrayOfWord);
+    }
+
+  },
+  mounted() {
+    this.arrayDimension(4);
+    this.logOfArray();
+  }
 }
 </script>
 <style lang="">
